2954de414a384a717f7fe6e5fdd57d1fcad3b623
Priyansh1411 <priyanshmishra7965@gmail.com>
2026-02-27 09:40:20 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main (String[] args)
     {
-        System.out.println("Hello Palindrome");
+        // Create Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Accept input from user
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        // Close scanner
+        scanner.close();
     }
 }
